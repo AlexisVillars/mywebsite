c8acd94d3f7d91703890813e9042cf30a9b94486
Alexis <villars.alexis@gmail.com>
2022-08-18 16:12:29 +0200

    start media queries

--- js/index.js
+++ js/index.js
@@ -1,19 +1,38 @@
-const ratio = 0.2;
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.2
-}
+document.addEventListener("DOMContentLoaded", function (event) {
+    const ratio = 0.2;
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2
+    }
 
-const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.intersectionRatio > ratio) {
-            entry.target.classList.add('reveal-visible')
-        }
+    const handleIntersect = function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > ratio) {
+                entry.target.classList.add('reveal-visible')
+            }
+        })
+    }
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+    document.querySelectorAll('.reveal').forEach(function (r) {
+        observer.observe(r);
     })
-}
 
-const observer = new IntersectionObserver(handleIntersect, options);
-document.querySelectorAll('.reveal').forEach(function (r) {
-    observer.observe(r);
+
+    const handleIntersectBar = function (entries) {
+        entries.forEach(function (entry) {
+            if (entry.intersectionRatio > ratio) {
+                entry.target.classList.add('progress')
+            }
+        })
+    }
+
+    const observerBar = new IntersectionObserver(handleIntersectBar, options);
+    document.querySelectorAll('.reveal-bar').forEach(function (r) {
+        observerBar.observe(r);
+    })
 })
+
+
+
