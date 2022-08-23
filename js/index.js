11d9405daf12934c9a4a766b105cc2e17bc77291
Alexis <villars.alexis@gmail.com>
2022-08-23 12:59:02 +0200

    burger menu for mobile

--- js/index.js
+++ js/index.js
@@ -32,6 +32,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelectorAll('.reveal-bar').forEach(function (r) {
         observerBar.observe(r);
     })
+
+    var sidenav = document.getElementById("mySidenav");
+    var openBtn = document.getElementById("openBtn");
+    var closeBtn = document.getElementById("closeBtn");
+
+    openBtn.onclick = openNav;
+    closeBtn.onclick = closeNav;
+
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        sidenav.classList.add("active");
+    }
+
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        sidenav.classList.remove("active");
+    }
 })
 
 
